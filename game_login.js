c58cf59feb786679fd0a608ebfbd90aff1910d53
Sriamsh <100685282+Sriamsh@users.noreply.github.com>
2022-08-09 20:05:11 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,11 @@
+function login()
+{
+    player1name=document.getElementById("player1nameinput").value;
+    player2name=document.getElementById("player2nameinput").value;
+
+    localStorage.setItem("player1name",player1name);
+    localStorage.setItem("player2name",player2name);
+
+    window.location="game_page.html";
+    
+}
